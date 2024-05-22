9a2343ef4f6973b0bc2494a60c305c73ceacd624
Regard88 <Regarty88@gmail.com>
2024-05-22 12:08:52 +0800

    Main Menu SFX Button Update

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -3,21 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public AudioSource SFX;
+
     public void StartGame()
     {
         // Load the main game scene
         SceneManager.LoadScene("Scene 1");
+        SFX.Play();
     }
 
     public void ExitGame()
     {
         // Quit the application (works in standalone builds)
         Application.Quit();
+        SFX.Play();
+    }
+
+    public void Option()
+    {
+        SFX.Play();
     }
 
     public void ShowHelp()
     {
         // Load the main game scene
         SceneManager.LoadScene("Help");
+        SFX.Play();
     }
 }
